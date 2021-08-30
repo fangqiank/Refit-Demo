d91563cede1cc4fab03e648b302b77f16eec485b
naiq f <fangqiank@outlook.com>
2021-08-30 09:06:21 -0600

    final

---
+++ SimpleUI/DataAccess/IWeatherData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Refit;
+using SimpleUI.Models;
+
+namespace SimpleUI.DataAccess
+{
+    public interface IWeatherData
+    {
+        [Get("/WeatherForecast")]
+        Task<List<WeatherModel>> GetWeather();
+        
+    }
+}
